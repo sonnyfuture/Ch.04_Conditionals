e69b23315646f0d08adaddbc4077394cc10ffe6e
agicedd <agicedd@urbandaleschools.com>
2019-01-09 12:56:44 -0600

    quiz master

--- 4.2_Quiz_Master.py
+++ 4.2_Quiz_Master.py
@@ -5,4 +5,24 @@ The criteria for the project are on the website. Make sure you test this quiz wi
 two of your student colleagues before you run it by your instructor.
 '''
 
-int(input())
+question_1=int(input("What is 1+1? "))
+if question_1 == 2:
+    print ("Congrats on knowing basic math")
+else:
+    print ("How?")
+
+question_2=(input("Who is Naruto's son? "))
+if question_2.lower() == "boruto":
+    print ("You are a man of culture.")
+else:
+    print ("Watch Naruto nerd.")
+
+question_3=(input("What is the best Star Wars movie? A) Episode 4: A New Hope B) Episode 5: The Empire Strikes Back C) Episode 6: Return of the Jedi or D) Episode 8: The Last Jedi? "))
+if question_3.lower() == "a":
+    print ("Ehh, maybe.")
+if question_3.lower() == "b":
+    print ("Yeah, Probably.")
+if question_3.lower() == "c":
+    print ("Probably most definitely not.")
+if question_3.lower() == "d":
+    print ("Why do you feel this way? Get some help.")

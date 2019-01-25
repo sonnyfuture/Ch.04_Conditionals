ea42c03c409880795fc7b16c25a8017e4b4ecf05
agicedd <agicedd@urbandaleschools.com>
2019-01-25 12:17:47 -0600

    quiz master

--- 4.2_Quiz_Master.py
+++ 4.2_Quiz_Master.py
@@ -5,24 +5,46 @@ The criteria for the project are on the website. Make sure you test this quiz wi
 two of your student colleagues before you run it by your instructor.
 '''
 
-question_1=int(input("What is 1+1? "))
-if question_1 == 2:
+
+
+score = 0
+import time
+now=time.time()
+print(now)
+
+question_1=(input("What is 1+1? "))
+if question_1 == "2":
     print ("Congrats on knowing basic math.")
+    score+=1
 else:
     print ("HOW COULD YOU EVEN MESS THAT UP?")
 
 question_2=(input("Who is Naruto's son? "))
 if question_2.lower() == "boruto":
     print ("You are a man of culture.")
+    score += 1
 else:
     print ("If you haven't seen Naruto you HAVE NOT LIVED.")
 
 question_3=(input("What is the best Star Wars movie? A) Episode 4: A New Hope B) Episode 5: The Empire Strikes Back C) Episode 6: Return of the Jedi or D) Episode 8: The Last Jedi? "))
 if question_3.lower() == "a":
     print ("Ehh, maybe.")
-if question_3.lower() == "b":
+elif question_3.lower() == "b":
     print ("Yeah, Probably.")
-if question_3.lower() == "c":
+    score += 1
+elif question_3.lower() == "c":
     print ("Probably most definitely not.")
-if question_3.lower() == "d":
+elif question_3.lower() == "d":
     print ("Why do you feel this way? Get some help.")
+
+question_4=(input("What is the name of the most common commercially sold banana? "))
+if question_4=="Cavendish":
+    print("You is banana god.")
+    score+=1
+else:
+    print("No. It is a Cavendish.")
+
+question_5=(input("How many days has the government been shut down? "))
+
+print(score)
+
--- Grade Program 2.0.py
+++ Grade Program 2.0.py
@@ -5,17 +5,17 @@ exam_weight = int(input("Final Exam Weight: "))
 final_grade=(((100-exam_weight)*current+(exam_weight*final_exam))/100)
 print (final_grade)
 
-if final_grade >= 90 and final_grade <= 100:
+if final_grade >= 90:
     print ("A")
 
-if final_grade >= 80 and final_grade < 90:
+elif final_grade >= 80:
     print ("B")
 
-if final_grade >= 70 and final_grade < 80:
+elif final_grade >= 70:
     print ("C")
 
-if final_grade >= 60 and final_grade < 70:
+elif final_grade >= 60:
     print ("D")
 
-if final_grade >= 0 and final_grade < 60:
-    print ("F... Transfer to Johnston")
+else:
+    print("F... Transfer to Johnston.")
